a399ca02362cbefa7ab2240888fd6f2703b1cd38
Juan Mena Fernandez <juanmena95@hotmail.es>
2026-02-18 03:06:47 -0800

    Base paths now defined on an external script and loaded in notebooks

---
+++ notebooks/paths.py
@@ -0,0 +1,4 @@
+import os
+
+code_path = f"{os.environ['PSCRATCH']}/BAOfit_wtheta_wiggle-only_alpha" # wherever you cloned BAOfit_wtheta
+save_path = f"{os.environ['PSCRATCH']}/BAOfit_wtheta_wiggle-only_alpha" # wherever you want to save outputs
